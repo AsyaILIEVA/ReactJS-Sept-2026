d78d6c5659302ba6ff833e3be7bf43ab265052d3
AsyaILIEVA <asyailieva9@gmail.com>
2026-09-12 23:27:27 +0100

    ReactJS-Demo-JSX

--- jsx-demo/src/index.js
+++ jsx-demo/src/index.js
@@ -1,28 +1,9 @@
-console.log("Hello, React!");
+const root = ReactDOM.createRoot(document.getElementById('root'));
 
-// Create DOM Element
-const h1DomElement = document.createElement('h1');
-h1DomElement.textContent = 'Hello from DOM element';
+root.render(
+    <header>
+        <h1>Hello from JSX</h1>
+        <h2>The best UI ever!</h2>
+    </header>
+);
 
-// Render DOM Element
-document.body.appendChild(h1DomElement);
-console.dir(h1DomElement);
-
-// Create React Element
-const h1ReactElement = React.createElement('h1', null, 'Hello from React Element!');
-
-// Select DOM Element for React root container
-const rootElement = document.getElementById('root');
-const rootReactElement = ReactDOM.createRoot(rootDomElement);
-
-// Create secondary header react element
-const h2ReactElement = React.createElement('header', null, 'The best UI library ever!');
-
-// Create header element
-const headerReactElement = React.createElement('header', null, h1ReactElement, h2ReactElement);
-
-
-// Render elements inside the root
-rootReactElement.render(headerReactElement);
-
-console.dir(h1ReactElement);
