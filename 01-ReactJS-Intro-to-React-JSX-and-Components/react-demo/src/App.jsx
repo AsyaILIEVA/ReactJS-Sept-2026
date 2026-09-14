04365fb8d177f15a773d80a5ab656425ebf564da
AsyaILIEVA <asyailieva9@gmail.com>
2026-09-14 22:40:16 +0100

    ReactJS-Demo-React-App.jsx-Function declaration - 3 ways

--- 01-ReactJS-Intro-to-React-JSX-and-Components/react-demo/src/App.jsx
+++ 01-ReactJS-Intro-to-React-JSX-and-Components/react-demo/src/App.jsx
@@ -1,7 +1,32 @@
 import './App.css'
 
+//Function declaration component example
 function App() {  
-  return <h1>Hello, React!</h1>   
+  return (
+    <div>
+      <header>
+        <h1>Hello, React!</h1>  
+        <h2>Welcome to my React App!</h2>
+      </header>
+      <main>
+        <p>This is the main content of the app.</p>
+      </main>
+    </div>
+  ); 
 }
+
+// //Arrow function component example
+// const App = () => {
+//   const reactElement = <h1>Hello, React!</h1>
+
+//   return reactElement
+// }
+
+// //Function expression component example
+// const App = function() {
+//   const reactElement = <h1>Hello, React!!!!!!!</h1>
+
+//   return reactElement
+// }
 
 export default App
